2488753a08f3e54c0811d98a5ab2f585fb012ce5
Jonathan Downs <jondowns@vt.edu>
2013-10-07 12:52:31 -0400

    Everything should be added.

---
+++ Dots_Project/src/com/mad/dots/MainDots.java
@@ -0,0 +1,31 @@
+package com.mad.dots;
+
+import sofia.graphics.OvalShape;
+import sofia.app.*;
+
+// -------------------------------------------------------------------------
+/**
+ * The main class for the Dots game. This contains the commands for the main
+ * dots screen.
+ *
+ * @author Jonathan Downs
+ * @author Matthew Gross
+ * @author Jason Hall
+ * @version 0.1
+ */
+public class MainDots
+    extends ShapeScreen
+{
+    private OvalShape dot;
+
+
+    /**
+     * Sets up elements of the class when this screen first pops up.
+     */
+    public void initialize()
+    {
+        // To be implemented.
+    }
+
+    // To be implemented.
+}
