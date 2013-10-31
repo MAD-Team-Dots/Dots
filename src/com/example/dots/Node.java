29928c218a7cc4820e635a9dcbd009f11b8bba78
Sillyace <ezargarpur@gmail.com>
2013-10-30 21:14:27 -0400

    Node

---
+++ src/com/example/dots/Node.java
@@ -0,0 +1,13 @@
+package com.example.dots;
+
+public class Node<T>
+{
+    private T data;
+
+    public Node(T data) {
+        this.data = data;
+    }
+    public T data() {
+        return data;
+    }
+}
